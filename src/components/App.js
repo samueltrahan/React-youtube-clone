1897d03bbeeed09f40ec35e92f322612e3957350
Samuel Trahan <samtrahan91@gmail.com>
2020-08-12 19:09:10 -0500

    video list and video item add

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import youtube from '../apis/youtube'
+import youtube from "../apis/youtube";
+import VideoList from './VideoList'
 
-const KEY = 'AIzaSyB6SQk5JhQ-n5iwZ2gJE4tOT5d9O86UDGM';
+const KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
-        part: 'snippet',
-        type: 'video',
+        part: "snippet",
+        type: "video",
         maxResults: 5,
         key: KEY,
-        q: term
-      }
-    })
-  }
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

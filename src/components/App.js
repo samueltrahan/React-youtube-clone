2b40fba9c12248d2f70309c0c3b177ad6f5683c6
Samuel Trahan <samtrahan91@gmail.com>
2020-08-13 21:20:34 -0500

    video page styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
-import VideoList from './VideoList'
-import VideoDetail from './VideoDetail'
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 const KEY = process.env.REACT_APP_API_KEY;
 
@@ -22,16 +22,27 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
-onVideoSelect = (video) => {
-  this.setState({selectedVideo: video})
-}
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

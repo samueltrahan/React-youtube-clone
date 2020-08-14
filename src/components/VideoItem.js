85564ed4ebb511d03556431a9f654e5bb9640d4e
Samuel Trahan <samtrahan91@gmail.com>
2020-08-13 19:44:53 -0500

    video styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import './VideoItem.css'
 
-
-const VideoItem = ({video}) => {
+const VideoItem = ({ video }) => {
   return (
-  <div>
-    <img src={video.snippet.thumbnails.medium.url}/>
-    {video.snippet.title};
-  </div>
-  )
-}
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title};</div>
+      </div>
+    </div>
+  );
+};
 
-export default VideoItem
+export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = ({videos}) => {
     return <VideoItem video={video}/>
   })
   
-  return <div>
+  return <div className="ui relaced divided list">
     {renderedList}
   </div>
 }

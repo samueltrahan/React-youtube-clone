879e87a83accbfd4a96e07f5f3af8491b1591900
Samuel Trahan <samtrahan91@gmail.com>
2020-08-13 19:36:50 -0500

    video thumbnail showing

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 
-const VideoItem = (props) => {
-  return <div>Video List</div>
+const VideoItem = ({video}) => {
+  return (
+  <div>
+    <img src={video.snippet.thumbnails.medium.url}/>
+    {video.snippet.title};
+  </div>
+  )
 }
 
 export default VideoItem
